bab5a487103d6fe42897591a135f84c8b3b41dd7
platisam <platisam@gmail.com>
2023-07-07 01:02:53 +0200

    imported global style

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 const HomePage = () => {
   return (
     <div>
       <PageNav />
-
+      <AppNav />
       <h1>WorldWise</h1>
 
       <Link to="/app">Go to the app</Link>
