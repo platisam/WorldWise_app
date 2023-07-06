8e0ad649750048a7a6f7a052e175e127e844a441
platisam <platisam@gmail.com>
2023-07-06 18:20:42 +0200

    importing BrowserRouter, Route and Routes and setting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
