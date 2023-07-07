b4fd4a662cf28c28b5b5ef35c405c7f6128f2649
platisam <platisam@gmail.com>
2023-07-07 15:46:55 +0200

    created nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,11 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
