b19c4a9b246ebf57967b877d8757a5c7d134529d
platisam <platisam@gmail.com>
2023-07-07 23:23:40 +0200

    displaying the id using the useParams hook

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,6 +10,8 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -19,7 +22,7 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City</h1>;
+  return <h1>City {id}</h1>;
   /*
   return (
     <div className={styles.city}>
