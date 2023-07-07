3776c900ecdeab32aa50c707733e246416c37e2c
platisam <platisam@gmail.com>
2023-07-07 16:06:00 +0200

    created index route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
 import Logo from "./Logo";
@@ -9,7 +10,8 @@ const Sidebar = () => {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
+
       <Footer />
     </div>
   );
