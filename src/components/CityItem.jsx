528535e41a1485816d64cdf562d4227301d36ee1
platisam <platisam@gmail.com>
2023-07-07 19:11:30 +0200

    adding formatDate function for formatting date

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,13 @@
 import styles from "./CityItem.module.css";
 
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    //weekday: "long",
+  }).format(new Date(date));
+
 const CityItem = ({ city }) => {
   const { cityName, date, emoji } = city;
 
@@ -7,7 +15,7 @@ const CityItem = ({ city }) => {
     <li className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{date}</time>
+      <time className={styles.date}>{formatDate(date)}</time>
     </li>
   );
 };
