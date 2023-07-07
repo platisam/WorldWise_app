06d05a0e47a9d12b64e1f99b9ee5ea64040c5ca9
platisam <platisam@gmail.com>
2023-07-07 19:08:04 +0200

    destructuring city object and displaying cityName, date and emoji in UI

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,4 +1,14 @@
+import styles from "./CityItem.module.css";
+
 const CityItem = ({ city }) => {
-  return <li>city</li>;
+  const { cityName, date, emoji } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{date}</time>
+    </li>
+  );
 };
 export default CityItem;
