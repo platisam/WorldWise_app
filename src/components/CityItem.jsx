f7cb46b3b2d3a74a893bdb3965e584c5b7863420
platisam <platisam@gmail.com>
2023-07-07 23:08:10 +0200

    dynamically added id to the Link in the CityItem component

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -10,11 +10,11 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 const CityItem = ({ city }) => {
-  const { cityName, date, emoji } = city;
+  const { cityName, date, emoji, id } = city;
 
   return (
     <li>
-      <Link className={styles.cityItem}>
+      <Link className={styles.cityItem} to={`${id}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
