cec74b78197ced1a436625e6a7dd32854224e3cd
platisam <platisam@gmail.com>
2023-07-07 18:02:13 +0200

    fetching data from json-server with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,29 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "http://localhost:8000";
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
