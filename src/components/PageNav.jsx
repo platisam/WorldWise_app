bba58e8c18ad82bd824c1aa37fa6938cedb2e276
platisam <platisam@gmail.com>
2023-07-07 13:37:55 +0200

    components folder setup

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -15,7 +15,9 @@ const PageNav = () => {
           <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
