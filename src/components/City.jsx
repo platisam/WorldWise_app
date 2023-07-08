09cc0998646996d3a1c8a6d28582fdbbceea77c7
platisam <platisam@gmail.com>
2023-07-08 13:53:09 +0200

    adding query string params to City component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -11,6 +12,9 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   // TEMP DATA
   const currentCity = {
@@ -22,7 +26,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
   /*
   return (
     <div className={styles.city}>
