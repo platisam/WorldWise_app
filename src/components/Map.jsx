4d6f717e4954d60948c4a678dd2eb0f3436c4ab7
platisam <platisam@gmail.com>
2023-07-10 23:24:15 +0200

    storing data on click in the URL

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -67,7 +67,7 @@ function DetectClick() {
   const navigate = useNavigate();
 
   useMapEvents({
-    click: (e) => navigate("form"),
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
   });
 }
 
