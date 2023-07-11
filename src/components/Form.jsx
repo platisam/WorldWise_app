f93f5e9f6c9f93957813bb30cbeeba216d0ad84b
platisam <platisam@gmail.com>
2023-07-11 22:51:44 +0200

    adding onSubmit event and handleSubmit function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -59,6 +59,10 @@ function Form() {
     fetchCityData();
   }, [lat, lng]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   if (isLoadingGeocoding) return <Spinner />;
 
   if (!lat && !lng)
@@ -67,7 +71,7 @@ function Form() {
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
