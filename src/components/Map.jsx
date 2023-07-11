a41bcdcda8c8e5a83917bcee826379d23566bcbd
platisam <platisam@gmail.com>
2023-07-11 14:26:44 +0200

    created useUrlPosition custom hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -17,15 +17,11 @@ import Button from "./Button";
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
 
   useEffect(
     function () {
