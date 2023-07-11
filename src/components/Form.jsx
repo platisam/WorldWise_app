5d483376b7b72115cb6af69b1d37ac3ceba8dcdd
platisam <platisam@gmail.com>
2023-07-11 17:10:30 +0200

    setting Error for geocoding

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,20 +25,29 @@ function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
-
   const [emoji, setEmoji] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(() => {
     async function fetchCityData() {
       try {
         setIsLoadingGeocoding(true);
+        setGeocodingError("");
+
         const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
         const data = await res.json();
         console.log(data);
+
+        if (!data.countryCode)
+          throw new Error(
+            "That doesn't seem to be a city. Click somewhere else 🫡"
+          );
+
         setCityName(data.city || data.locality || "");
         setCountry(data.countryName);
         setEmoji(convertToEmoji(data.countryCode));
-      } catch (error) {
+      } catch (err) {
+        setGeocodingError(err.message);
       } finally {
         setIsLoadingGeocoding(false);
       }
