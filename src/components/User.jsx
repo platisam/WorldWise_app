45223cc2688d188a03c7025a9bb6872b03beafcb
platisam <platisam@gmail.com>
2023-07-13 14:01:47 +0200

    imported User component in AppLayout page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,14 +1,8 @@
+import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function User() {
-  const user = FAKE_USER;
+  const { user } = useAuth();
 
   function handleClick() {}
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
   }
 
   useEffect(() => {
-    if (isAuthenticated) navigate("/app");
+    if (isAuthenticated) navigate("/app", { replace: true });
   }, [isAuthenticated, navigate]);
 
   return (
