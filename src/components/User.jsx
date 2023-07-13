48096ecf6590f7d4dfa575bc5d38eda4abc31650
platisam <platisam@gmail.com>
2023-07-13 14:09:23 +0200

    setting logout function in User component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./User.module.css";
 
 function User() {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
