0d135b11c8b1dac4483cb77636f2bc69a6507996
platisam <platisam@gmail.com>
2023-07-13 15:34:35 +0200

    wrapped the entire AppLayout in a ProtectedRoute

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,10 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
-  return children;
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+
+  return isAuthenticated ? children : null;
 };
 export default ProtectedRoute;
