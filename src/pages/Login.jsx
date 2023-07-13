5e823727a8bb37edd1cce799c76a30d7bc4482b7
platisam <platisam@gmail.com>
2023-07-13 12:31:59 +0200

    adding AuthProvider to App component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,3 +38,17 @@ export default function Login() {
     </main>
   );
 }
+
+/*
+CHALLENGE
+
+1) Add `AuthProvider` to `App.jsx`
+2) In the `Login.jsx` page, call `login()` from context
+3) Inside an effect, check whether `isAuthenticated === 
+true`. If so, programatically navigate to `/app`
+4) In `User.js`, read and display logged in user from 
+context (`user` object). Then include this component in 
+`AppLayout.js`
+5) Handle logout button by calling `logout()` and 
+navigating back to `/`
+*/
