00d6da33d48fa5808bab9b12b0b31c3a56923187
platisam <platisam@gmail.com>
2023-07-13 13:37:37 +0200

    setting useEffect for programmatic navigation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
 import { useAuth } from "../contexts/FakeAuthContext";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
-  const { login } = useAuth();
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
 
     if (email && password) login(email, password);
   }
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/app");
+  }, [isAuthenticated, navigate]);
 
   return (
     <main className={styles.login}>
