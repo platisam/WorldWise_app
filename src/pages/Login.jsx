a10dbbc1467249fc97777f7840213ad7c2d0a745
platisam <platisam@gmail.com>
2023-07-13 13:22:50 +0200

    calling login function in the Login component and setting onSubmit event

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
+import { useAuth } from "../contexts/FakeAuthContext";
+import Button from "../components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  const { login } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +42,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
